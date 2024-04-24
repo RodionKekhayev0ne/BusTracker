d91d50a9fa32d27d08eb431cd9d93f2da0a942df
Kekhayev Rodion <Kekhayev.r@mail.ru>
2024-04-24 20:10:32 +0600

    commit 24.04.24

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const regroutes = require('./registration');
 const procroutes = require('./process');
+const cors = require('cors');
 
+// Использование CORS middleware
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +14,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
+const corsOptions = {
+  origin: 'http://localhost:3000', // Разрешить запросы только с этого источника
+  methods: ['GET', 'POST'], // Разрешенные HTTP-методы
+  allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
+};
 
+// Использование настроенного CORS middleware
+app.use(cors(corsOptions));
 
   
   app.use('', regroutes);
